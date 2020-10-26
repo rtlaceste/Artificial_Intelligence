b463098f9adda7953fc949bc206f626069183efb
Troy Laceste <73194494+rtlaceste@users.noreply.github.com>
2020-10-25 18:19:33 -0700

    Add files via upload
    Added a base neural net

---
+++ Neural Net.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 25 15:24:22 2020
+
+@author: troy
+"""
+
+from numpy import loadtxt
+from keras.models import Sequential
+from keras.layers import Dense
+
+# load the dataset
+dataset = loadtxt('/Users/troy/Downloads/pima-indians-diabetes.data.csv', delimiter=',')
+# split into input (X) and output (y) variables
+X = dataset[:,0:8]
+y = dataset[:,8]
+
+# define the keras model
+model = Sequential()
+model.add(Dense(12, input_dim=8, activation='relu'))
+model.add(Dense(8, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+
+# compile the keras model
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# compile the keras model
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# fit the keras model on the dataset
+model.fit(X, y, epochs=150, batch_size=10)
+
+# evaluate the keras model
+_, accuracy = model.evaluate(X, y)
+print('Accuracy: %.2f' % (accuracy*100))
+
